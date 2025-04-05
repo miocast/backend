22bfff1b72d1fe3f48322c821e84320df2c260d2
miocast <rinatsoi05@gmail.com>
2025-04-05 20:30:51 +0300

    add endpoints on techSpec controller

---
+++ Contracts/GetTechnicalSpecRequest.cs
@@ -0,0 +1,6 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Contracts
+{
+    public record GetTechnicalSpecRequest(string? Search, [Required] int Size, [Required] int Page);
+}
